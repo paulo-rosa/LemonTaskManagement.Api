782e9ae74bda74d30f4ad40e3cab08f41cf039be
Paulo Douglas <paulo.douglas@fannexlive.com>
2025-11-09 15:29:10 -0600

    - Added assigned user property to the Card entity

--- LemonTaskManagement.Infra.Data/Configurations/CardConfiguration.cs
+++ LemonTaskManagement.Infra.Data/Configurations/CardConfiguration.cs
@@ -9,19 +9,25 @@ public class CardConfiguration : IEntityTypeConfiguration<Card>
     public void Configure(EntityTypeBuilder<Card> builder)
     {
         builder.HasKey(e => e.Id);
-        
+
         builder.Property(e => e.Description)
             .IsRequired()
             .HasMaxLength(400);
-        
+
         builder.Property(e => e.Order)
             .IsRequired();
-        
+
         builder.HasOne(e => e.BoardColumn)
             .WithMany(e => e.Cards)
             .HasForeignKey(e => e.BoardColumnId)
             .OnDelete(DeleteBehavior.Cascade);
-        
+
+        builder.HasOne(e => e.AssignedUser)
+            .WithMany()
+            .HasForeignKey(e => e.AssignedUserId)
+            .OnDelete(DeleteBehavior.SetNull)
+            .IsRequired(false);
+
         builder.HasIndex(e => new { e.BoardColumnId, e.Order });
     }
 }
